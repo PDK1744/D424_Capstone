bc1e11b0d8113422c6e2d4b58c931a7640e78a03
Kobe <kbonn43@wgu.edu>
2024-10-01 11:01:26 -0500

    Added missed route

--- server.js
+++ server.js
@@ -23,3 +23,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log('Server running on port ${PORT}');
 });
+
+const itemRoutes = require('./routes/itemRoutes');
+
+app.use('/api/items', itemRoutes);
